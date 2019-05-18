c6ae9f0760065a81db10721c5c737e916aedc801
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-17 18:24:57 -0700

    catch and print exceptions for title parsing

--- process.py
+++ process.py
@@ -19,8 +19,12 @@ for year in filter(os.path.isdir, years):
             for filename in os.listdir(os.path.join(year, month, day)):
                 file_path = os.path.join(year, month, day, filename)
                 with open(file_path, "r") as file:
-                    _, title = file.readline().split("# ")
-                title = title.strip()[:MAX_TITLE_LENGTH]
+                    try:
+                        _, title = file.readline().split("# ")
+                        title = title.strip()[:MAX_TITLE_LENGTH]
+                    except Exception as e:
+                        print("Error parsing title in file", file_path, e)
+                        continue
                 encoded_title = base64.urlsafe_b64encode(title.encode("utf-8")).decode("utf-8")
                 new_path = os.path.join(year, month, day, filename)
                 new_filename = filename

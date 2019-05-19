3e24b7ee6c55dac8fcff552e02119b502afd6f42
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-19 10:58:53 -0700

    change upload.sh to upload only a single repo

--- upload.sh
+++ upload.sh
@@ -1,12 +1,8 @@
 git config --global user.email "tech@stanforddaily.com"
 git config --global user.name "The Stanford Daily Bot"
-ls | grep "xx" | while read century ; do
-    ls $century | grep "x" | while read decade; do
-        rm -rf .git
-        git init
-        git remote add origin git@bitbucket.org:thestanforddaily/$decade.git
-        git add $century/$decade
-        git commit -m "Updates"
-        git push -f origin HEAD:master
-    done
-done
+rm -rf .git
+git init
+git remote add origin git@bitbucket.org:thestanforddaily/$decade.git
+git add $century/$decade
+git commit -m "Updates"
+git push -f origin HEAD:master

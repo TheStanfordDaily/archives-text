429495de749de0065e0c35827aedf4b65573f56c
Ubuntu <ubuntu@tsd-compute.2xvncoysd25u1i5faxeftilkic.gx.internal.cloudapp.net>
2019-05-19 01:39:18 +0000

    add corrections

---
+++ corrections.py
@@ -0,0 +1,35 @@
+import xml.etree.ElementTree as ET
+import os
+import re
+import fileinput
+
+PATH = "./stanford-text-corrections/stanford"
+
+def create_corrections():
+    years = os.listdir(PATH)
+    for year in filter(os.path.isdir, years):
+        for directory in os.listdir(os.path.join(PATH, year)):
+            for (dirname, year, month, day) in re.findall(r'((\d{4})(\d{2})(\d{2})-\d{2})', directory):
+                path = os.path.join("./corrections", year, dirname + ".dir", f"stanford{dirname}-changes.log")
+                with open(path) as f:
+                    xml = f.read()
+                tree = ET.fromstring("<root>" + xml + "</root>")
+                # print(tree, year, month, day)
+                corrections = []
+                files = [os.path.join(year, month, day, file) for file in os.listdir(os.path.join(year, month, day))]
+                for textCorrectedBlock in tree:
+                    blockID = textCorrectedBlock.attrib["blockID"]
+                    corrections = []
+                    for textCorrectedLine in textCorrectedBlock:
+                        oldTextValue = textCorrectedLine.findtext("OldTextValue")
+                        newTextValue = textCorrectedLine.findtext("NewTextValue").strip()
+                        corrections.append((oldTextValue, newTextValue))
+                yield files, corrections
+
+for files, corrections in create_corrections():
+    print(files, corrections)
+    with fileinput.input(files=(files), inplace=True) as f:
+        for line in f:
+            if line.strip() in corrections:
+                print(line)
+    break
